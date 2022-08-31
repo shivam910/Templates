5be77ad0cba719a28a454c104aba106830cf8fdc
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2022-08-31 16:02:01 +0530

    BMS

---
+++ BMS website development service page/script.js
@@ -0,0 +1,37 @@
+$('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    centerMode: true,
+    variableWidth: true,
+    infinite: true,
+    focusOnSelect: true,
+    cssEase: 'linear',
+    touchMove: true,
+    prevArrow:'<button class="slick-prev"> < </button>',
+    nextArrow:'<button class="slick-next"> > </button>',
+    
+            // responsive: [                        
+            //     {
+            //       breakpoint: 576,
+            //       settings: {
+            //         centerMode: false,
+            //         variableWidth: false,
+            //       }
+            //     },
+            // ]
+  });
+  
+  
+  var imgs = $('.slider img');
+  imgs.each(function(){
+    var item = $(this).closest('.item');
+    item.css({
+      'background-image': 'url(' + $(this).attr('src') + ')', 
+      'background-position': 'center',            
+      '-webkit-background-size': 'cover',
+      'background-size': 'cover', 
+    });
+    $(this).hide();
+  });
